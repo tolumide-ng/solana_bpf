ee8aa93f4293078346f145472b4574ee1f8c5c1e
tolumide-ng <tolumideshopein@gmail.com>
2021-12-18 17:09:20 +0100

    Implements 'from' for ProgramError to enable conversion from EscrowError

--- src/error.rs
+++ src/error.rs
@@ -1,4 +1,5 @@
 use thiserror::Error;
+use solana_program::program_error::ProgramError;
 
 #[derive(Error, Debug, Copy, Clone)]
 pub enum EscrowError {
@@ -7,3 +8,9 @@ pub enum EscrowError {
     InvalidInstruction
 }
 
+
+impl From<EscrowError> for ProgramError {
+    fn from(e: EscrowError) -> Self {
+        ProgramError::Custom(e as u32)
+    }
+}
