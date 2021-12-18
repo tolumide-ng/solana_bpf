84a65a23a78adb89a2ef8853d2b55d5a0bfbabb2
tolumide-ng <tolumideshopein@gmail.com>
2021-12-18 12:25:12 +0100

    creates entrypoint module

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,3 @@
-use solana_program::{
-    account_info::AccountInfo, entrypoint, entrypoint::ProgramResult, msg, pubkey::Pubkey,
-};
+pub mod entrypoint;
 
-entrypoint!(process_instruction);
-fn process_instruction(
-    program_id: &Pubkey,
-    accounts: &[AccountInfo],
-    instruction_data: &[u8],
-) -> ProgramResult {
-    msg!(
-        "process_instruction: {}: {} accounts, data={:?}",
-        program_id,
-        accounts.len(),
-        instruction_data
-    );
-    Ok(())
-}
+
